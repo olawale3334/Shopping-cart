b6711f135ef9ed18141e1781882bae7ed4b7cfc0
olawale3334 <olawale3334@gmail.com>
2025-08-03 09:45:07 +0100

    Initial commit: setup empty React project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+import NavBar from './Components/NavBar'
+import HeroPage from './Components/HeroPage'
+function App() {
+ 
+
+  return (
+    <>
+     <NavBar/>
+     <HeroPage/>
+    </>
+  )
+}
+
+export default App
